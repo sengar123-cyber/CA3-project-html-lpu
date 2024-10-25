9b1a0428fb7013178a0a00de8f4c6ff28582b14f
sengar123-cyber <sengarkartikey3@gmail.com>
2024-10-25 16:18:41 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,29 @@
+function myfun(){
+  let searchval=document.getElementById("inp").value.toUpperCase();
+  let ul=document.getElementsByTagName("ul")
+  let li=document.getElementsByTagName("li");
+
+  for (let i=0; i<li.length; i++){
+    let 
+  }
+  console.log(searchval);
+}
+
+
+    function data()
+    {
+    var a=document.getElementById("name").value;
+    var b=document.getElementById("email").value;
+    var c=document.getElementById("phone").value;
+    var d=document.getElementById("message").value;
+    if(a==""||b==""||c==""|| d=="")
+    {
+        alert("All fields are manadatory");
+        return false;
+    }
+    else{
+        true;
+    }
+    }
+
+
